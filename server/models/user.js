f37b7a01076ba10ef99b13e411228d5a5da1a803
angyalzsolt <angyal.zsolt@hotmail.hu>
2019-02-19 00:27:34 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -71,7 +71,24 @@ UserSchema.statics.findByToken = function(token) {
 		'tokens.token': token,
 		'tokens.access': 'auth'
 	});
-}
+};
+
+
+UserSchema.statics.findByCredentials = function(email, password) {
+	let User = this;
+	return User.findOne({email}).then((user)=> {
+		if(!user){
+			return Promise.reject();
+		};
+		return bcrypt.compare(password, user.password).then((res) => {
+			if(res){
+				return user;
+			}else{
+				return Promise.reject();
+			}
+		});	
+	})
+};
 
 
 
